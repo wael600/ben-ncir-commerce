897d2c1cd97b7829d086e96d8d622b7f7fc84d3b
wael600 <yaelwael@gmail.com>
2026-05-29 13:00:19 +0200

    fix db adapter for postgresql

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,7 @@
 import { PrismaClient } from "@/app/generated/prisma/index.js";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 const PrismaClientSingleton = () => {
-  const adapter = new PrismaBetterSqlite3({
-    url: process.env.DATABASE_URL!,
-  });
-  return new PrismaClient({ adapter });
+  return new PrismaClient();
 };
 
 declare global {
@@ -13,7 +9,6 @@ declare global {
 }
 
 const db = globalThis.prisma ?? PrismaClientSingleton();
-
 export default db;
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
