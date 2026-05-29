eb97ef5747c3ae04869db0003d99b70a9e313402
wael600 <yaelwael@gmail.com>
2026-05-29 13:06:16 +0200

    fix prisma client import

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/app/generated/prisma/index.js";
+import { PrismaClient } from "@prisma/client";
 
 const PrismaClientSingleton = () => {
   return new PrismaClient();
