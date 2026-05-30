522a47450b687ac5eebe9f6d521c85d23b4573e9
wael600 <yaelwael@gmail.com>
2026-05-30 14:18:40 +0200

    fix cloudinary map

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,9 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "placehold.co",
+          hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
